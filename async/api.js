b9c4cb6eaf3ae378e167ae4ce16c17fa0ed88012
Muath <Muath.khalifa@yahoo.com>
2022-06-07 12:02:53 +0100

    final

--- async/api.js
+++ async/api.js
@@ -1,7 +1,9 @@
 https://api.advicesLip.com/advice
 
+
 fetch("https://api.advicesLip.com/advice")
-.then(result =>result.json()).then(data => console.log(data.slip.advice))
+.then(result =>result.json()).then(data => console.log(data))
 
-
-console.log("hi");console.log("hello");
+fetch("https://api.advicesLip.com/advice")
+.then(result => console.log(result))
+console.log("hi")
